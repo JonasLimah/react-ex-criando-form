41130ae0b362d096de5311beb5d1741d0d85f0d2
jonas Limah <jonaslimahpessoal@gmail.com>
2023-01-19 19:14:52 -0300

    meu primeiro formulario Reactjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
+import React,{useState} from "react";
 const App = () => {
+  const [name,setName] = useState("");
+  const [lastname,setLastname] = useState("");
+  const [age ,setAge] = useState("");
+  let handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  }
+  let handleLastName = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    setLastname(e.target.value)
+  }
+  let handleAge = (e: React.ChangeEvent<HTMLInputElement>) => {
+    
+      setAge(e.target.value)
+    
+    
+  }
+
   return (
     <div>
       <div>
         Nome:
-        <input type="text" />
+        <input type="text" value={name} onChange={handleName}/>
       </div>
 
       <div>
         Sobrenome:
-        <input type="text" />
+        <input type="text" value= {lastname} onChange={handleLastName}/>
       </div>
 
       <div>
         Idade:
-        <input type="text" />
+        <input type="text" maxLength={2} value={age} onChange={handleAge}/>
       </div>
 
       <hr/>
 
-      Olá ---, tudo bem?<br/>
-      Você tem -- anos.
+      Olá {name} {lastname}, tudo bem?<br/>
+      Você tem {age} anos.
     </div>
   );
 }
